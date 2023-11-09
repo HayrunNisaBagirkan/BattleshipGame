f8f6e1c2c9d0211f23bf0bd3f8fb12b4748f7629
HayrunNisaBagirkan <>
2023-11-09 01:18:58 +0000

    Added titles of the game planning

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# Board class
+# Game board and functions
+
+# Populates the boards wıth player'ss ships
+
+# Check if ships overlap
+
+# Places a ship on the board
+
+# Player makes a guess
+
+# Result of an attack on the board
+
+# Function for player to set up their own ships
+
+# Function to set up computer's ships randomly
+
+# Main function of the game
+
+# Start a new game

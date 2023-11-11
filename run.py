37fdb46efb8e9c7cff0634cd49a287e211d670ef
H.Nisa Bagirkan <h.nisabagirkan@gmail.com>
2023-11-11 01:25:56 +0000

    Added chart steps based on the sample project of CodeIns

---
+++ run.py
@@ -0,0 +1,61 @@
+"""
+Module and function
+"""
+
+
+"""
+Scoreboard
+"""
+
+
+"""
+Define Board class
+"""
+
+"""
+Define __init__ method to initialize a new game board.
+Set the board size, number of ships, player's name, the board type (player and computer)
+"""
+
+
+"""
+Print the current state of the game board. (print method)
+"""
+
+
+"""
+Set the method for player's guess and update the boards responses. (guesses method)
+"""
+
+
+"""
+Add ship to the game board. (add_ship)
+"""
+
+
+"""
+Generate random integer coordinates within the specified board size with 'random_point' function.
+"""
+
+
+"""
+Check if given coordinations are valid with 'valid_coordinates'function.
+"""
+
+
+"""
+Populate the game board with ships with 'populate_board' function.
+"""
+
+
+"""
+Set 'make_guess' and 'play_game' functions for players.
+"""
+
+
+"""
+Start a new game with 'new_game' function.
+Sets a new game, initializes the board size, number of ships, and player names.
+Creates a new game boards and score boards.
+"""
+

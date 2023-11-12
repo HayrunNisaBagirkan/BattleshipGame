8c1d47a27abecac9dde3895e2c5b97d93d2a8d04
H.Nisa Bagirkan <h.nisabagirkan@gmail.com>
2023-11-12 21:32:23 +0000

    Try to make the visual better with =

--- run.py
+++ run.py
@@ -206,11 +206,13 @@ def new_game():
     print("Welcome to Battleships Game!")
     print("Want to see how strong your predistions are?")
     print("Come on and try yourself against the computer.")
+    print("=" * 10)
     print(f"Board Size: {size}. Number of ships: {num_ships}")
     print("Top left corner is row: 0, col: 0")
     print("=" * 10)
 
     while True:
+        print("=" * 10)
         player_name = input("Please enter your name: ")
         if player_name:
             break
